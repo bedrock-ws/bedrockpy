5af5d8408d39271d07e49da06aa2aba71ca65ae1
phoenixr-codes <phoenixr41414141@gmail.com>
2023-06-09 16:46:25 +0200

    🖼️ Add logo & favicon

--- docs/conf.py
+++ docs/conf.py
@@ -88,3 +88,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'furo'
 html_static_path = ['_static']
 html_css_files = ['style.css']
+html_favicon = '_static/bedrockpy_3d.ico'
+html_logo = '../bedrockpy_3d.png'
