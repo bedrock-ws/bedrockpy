1b7e876b7100f28d601e6559aa5fc40285c2c8b5
phoenixr-codes <phoenixr41414141@gmail.com>
2023-06-09 15:34:13 +0200

    📖 Improve docs, fix mermaid graph

--- src/bedrock/consts.py
+++ src/bedrock/consts.py
@@ -1,23 +1,35 @@
 MAX_COMMAND_PROCESSING = 100
 """The amount of commands the client can process at a time without responding.
 
+This is already handleded internally so there is usually no need to use
+this. You can think of the client managing a FIFO[1]_ queue of command requests
+with a capacity of 100. The client pops out the first entered command request
+and sends a command response bback to the server. When the server tries to
+send a command request while the queue is already full, then the command will
+never be executed and an error will be sent by the client.
+
 .. mermaid::
-    caption: An example of an interaction between the server and the client.
+    :caption: An example of an interaction between the server and a client.
     
-    %%{init: {'theme': 'neutral'}}%%
+    %%{init: {'theme': 'dark'}}%%
     sequenceDiagram
         Server-)Client: Command Request
         Server-)Client: Command Request
         Server-)Client: Command Request
         Client-)Server: Command Response
-        Server-)Client: Command Request
-        Server-)Client: … 97 more …
+        loop 98 times
+            Server-)Client: Command Request
+        end
         Server-xClient: Command Request
-        Client-)Server: Command Response
-        Client-)Server: Command Response
-        Client-)Server: Command Response
-        Client-)Server: … 97 more …
+        loop 98 times
+        	Client-)Server: Command Response
+        end
 
+..[1] In computing and in systems theory, FIFO is an acronym for first in,
+    first out (the first in is the first out), a method for organizing
+    the manipulation of a data structure (often, specifically a data buffer)
+    where the oldest (first) entry, or "head" of the queue, is processed
+    first. (Wikipedia)
 """
 
 NAME = "External"
