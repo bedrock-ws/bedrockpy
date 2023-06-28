20b320b68b7202222541320ac44065d7d7bacf47
Jonas <99104369+phoenixr-codes@users.noreply.github.com>
2023-06-28 14:15:42 +0200

    🎨 Include Minecraft stylesheet

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'furo'
 html_title = 'bedrockpy'
 html_static_path = ['_static']
-html_css_files = ['style.css']
+html_css_files = ['style.css', 'mc.css']
 html_favicon = '_static/bedrockpy_3d.ico'
 html_logo = '../bedrockpy_3d.png'
