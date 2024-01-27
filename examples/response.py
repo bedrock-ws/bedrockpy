28889da3fcc006606fdb30d7adf8ad7e5c87aa9a
Jonas da Silva <phoenixr41414141@gmail.com>
2024-01-28 00:38:17 +0100

    Update example
    say triggers the event (now) again

--- examples/response.py
+++ examples/response.py
@@ -13,14 +13,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = Server()
 
+
 @app.server_event
 async def ready(ctx: ReadyContext) -> None:
     print(f"Ready @ {ctx.host}:{ctx.port}")
+
 
 @app.game_event
 async def player_message(ctx: PlayerMessageContext) -> None:
     if ctx.sender != NAME:
         res = await ctx.server.run(f"title {ctx.sender} title {ctx.message!r}")
-        await ctx.server.run(f"say {res!r}")
+        await ctx.server.run(f"title {ctx.sender} actionbar {res!r}")
+
 
 app.start(os.getenv("IP") or "localhost", 6464)
